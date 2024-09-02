11a15b5714870b2555a47ae698f1af451b076843
GDiiniiz <guilherme.diniz0056@gmail.com>
2024-09-02 15:18:13 -0300

    feat: add MongoGetUsersRepository

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import express from "express";
 import { config } from "dotenv";
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { GetUsersController } from "./controllers/get-users/get-users";
 config();
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Listening on port ${port}!`));
-
 app.get("/", (req, res) => {
   res.send("Hello world!!");
 });
+
+app.get("/users", async (req, res) => {
+  const mongoGetUsersRepository = new MongoGetUsersRepository();
+
+  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+
+  const { body, statusCode } = await getUsersController.handle();
+
+  res.status(statusCode).send(body);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
