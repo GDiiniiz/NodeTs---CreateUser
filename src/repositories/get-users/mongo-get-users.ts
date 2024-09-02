68e848b558d0fbca238405830d351513e03ea18c
GDiiniiz <guilherme.diniz0056@gmail.com>
2024-09-02 17:38:59 -0300

    feat: add id to User

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,8 +1,12 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/users";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db.collection<User>('users').find({}).toArray()
+
+    users[0].
     return [
       {
         firtsName: "Guilherme",
